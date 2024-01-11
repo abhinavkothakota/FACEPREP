164c9ad118edb4a836c15d110b7ce02d69a8ff56
Abhinav.K <145023443+abhinavkothakota@users.noreply.github.com>
2024-01-11 10:05:24 +0530

    starting

--- java/main.java
+++ java/main.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 class main{
     public static void main(String args[]){
-        Scanner scan=new Scanner(System.in);
-        String a=scan.nextLine();
-        int b =scan.nextInt();
-        System.out.println("NAME"+a);
+        String a,b;
+        a="ABHI";
+        b="k";
 
 
 
