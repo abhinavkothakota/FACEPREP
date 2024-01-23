4ed8b59513b7e12c33eb4c97c30002330677b61d
Abhinav.K <145023443+abhinavkothakota@users.noreply.github.com>
2024-01-23 22:12:35 +0530

    ntg

--- java/sup.java
+++ java/sup.java
@@ -1,10 +1,20 @@
-class sup{
-    public static void main(String args[]){
-        String a,b;
-        a="ABHI";
-        b="k";
+class sup{//string builder
+    public static void main(String[] args) {
+        
+    int one = 1;
+String color = "red";
+StringBuilder sb = new StringBuilder();
+sb.append("One=").append(one).append(", Color=").append(color).append('\n');
+System.out.print(sb);
 
-        System.out.println(a+b);
-
+/////String buffer
+System.out.println("\n");
+ String str = "study";
+ str.concat("tonight");
+ System.out.println(str); // Output: study
+ StringBuffer strB = new StringBuffer("study");
+ strB.append("tonight");
+ System.out.println(strB); // Output: studytonight
     }
 }
+
